221c48e3f596f8504b59d46d9375c6e09af4e4a9
adakolecodes <amdeezy.com@gmail.com>
2023-06-24 13:25:45 +0100

    new function created to interact with html

--- JavaScript/Projects/StatesLGAs/stateslgas.js
+++ JavaScript/Projects/StatesLGAs/stateslgas.js
@@ -1,18 +1,67 @@
+//Function to be used on console
+// function getLGAsByState(state) {
+//     // Define the LGA data
+//     const lgaData = {
+//         Benue: ["Markurdi", "Otukpo", "APA", "Ugbokolo"],
+//         Taraba: ["Jalingo", "Wukari", "Gembu", "Takum"],
+//         Abuja: ["FCT", "Gwags", "Kuje", "Lugbe"]
+//         // Add more states and LGAs as needed
+//     };
+  
+//     // Check if the state exists in the lga data
+//     if (state in lgaData) {
+//         return lgaData[state];
+//     } else {
+//         return []; // Empty array if the country is not found
+//     }
+// }
+
+// console.log(getLGAsByState("Benue"));
+
+
+
+
+//Function to be used on HTML
 function getLGAsByState(state) {
-    // Define the state data
+    // Define the LGA data
     const lgaData = {
         Benue: ["Markurdi", "Otukpo", "APA", "Ugbokolo"],
         Taraba: ["Jalingo", "Wukari", "Gembu", "Takum"],
         Abuja: ["FCT", "Gwags", "Kuje", "Lugbe"]
-        // Add more countries and states as needed
+        // Add more states and LGAs as needed
     };
+
+    //Get the lga data for the specific state parsed as an arguement and store in the lgas variable
+    const lgas = lgaData[state] || [];
   
-    // Check if the state exists in the lga data
-    if (state in lgaData) {
-        return lgaData[state];
-    } else {
-        return []; // Empty array if the country is not found
+    //Get the lga-container div tag/element
+    const lgaContainer = document.getElementById("lga-container");
+    // Clear previous content when another state is selected
+    lgaContainer.innerHTML = "";
+
+    //Check if the lga exists
+    if (lgas.length === 0) {
+        //Display a message by creating a p tag/element, setting the text content and appending it to the lga container div tag/element
+        //Create a p tag dynamically using JS
+        const message = document.createElement("p");
+        //Set the content for the p tag/element created (note: innterHTML or textContent can be used to set a content of an element/tag)
+        message.textContent = "No LGA found for the selected country.";
+        //Append the p tag/element to the lga container div tag/element
+        lgaContainer.appendChild(message);
+    }else {
+        //Create a ul tag/element dynamically using JS
+        const list = document.createElement("ul");
+        
+        //Loop through lgas and create an li tag/element for each
+        for (const lga of lgas) {
+            //Create a li tag/element dynamically using JS
+            const listItem = document.createElement("li");
+            //Set the text content for the li tag/element. Text content would be the iteration of each lga
+            listItem.textContent = lga;
+            //Append the li tag/element to the ul list created above
+            list.appendChild(listItem);
+        }
+        //Append the ul list to the lga container div tag/element
+        lgaContainer.appendChild(list);
     }
 }
-
-console.log(getLGAsByState("Benue"));
