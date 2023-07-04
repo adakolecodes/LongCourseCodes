f132a7c58c1c48fc956bec2c23148947c277ed6f
adakolecodes <amdeezy.com@gmail.com>
2023-07-04 09:00:14 +0100

    comments

--- JavaScript/Lessons/DictionaryApp/index.js
+++ JavaScript/Lessons/DictionaryApp/index.js
@@ -11,12 +11,12 @@ async function getMeaning(){
             const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${input}`;
             const response = await axios.get(url);
             const data = response.data[0];
-            console.log(response);
+            console.log(response.data);
 
             //Show meaning container when search is initiated
             document.getElementById("meaning-container").style.display = "block";
             
-            document.getElementById("word").innerHTML = data.word;
+            document.getElementById("word").innerHTML = data.word; //OR response.data[0].word
             document.getElementById("definition").innerHTML = data.meanings[0].definitions[0].definition;
             document.getElementById("audio").src = data.phonetics[0].audio;
 
