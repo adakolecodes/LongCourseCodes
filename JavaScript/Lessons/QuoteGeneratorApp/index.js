e19b04d4915b8fce17c8339660b6a187abaad221
adakolecodes <amdeezy.com@gmail.com>
2023-07-05 11:09:18 +0100

    Extra feature

--- JavaScript/Lessons/QuoteGeneratorApp/index.js
+++ JavaScript/Lessons/QuoteGeneratorApp/index.js
@@ -6,13 +6,22 @@ const apiUrl = "https://api.quotable.io/random";
 //To handle errors we use the try and catch block
 async function getQuote(){
     try{
+        //Set button to show loading and disabled when clicked
+        document.getElementById("btn").innerHTML = "Loading...";
+        document.getElementById("btn").disabled = true;
+
         const response = await axios.get(apiUrl);
         const data = response.data;
         console.log(data);
+
         document.getElementById("tags").innerHTML = data.tags;
         document.getElementById("content").innerHTML = data.content;
         document.getElementById("author").innerHTML = data.author;
         document.getElementById("error").innerHTML = "";
+
+        //After quotes have been loaded, enable button and show Generate
+        document.getElementById("btn").innerHTML = "Generate";
+        document.getElementById("btn").disabled = false;
     }catch(error){
         console.log(error);
         document.getElementById("error").innerHTML = error;
