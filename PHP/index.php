b115e338e9227a8a98af17e2b431c40b1b154f88
adakolecodes <amdeezy.com@gmail.com>
2024-05-04 04:00:28 +0100

    Html and php

---
+++ PHP/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $name = "John Doe";
+    ?>
+    <h1>Hello <?php echo $name; ?></h1>
+</body>
+</html>
